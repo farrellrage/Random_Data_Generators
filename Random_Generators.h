da484764618b558190b96afe33ad7e22a04031e5
Raging_Pigeon <austin.m.farrell@lmco.com>
2019-05-28 22:40:10 -0400

    Added function for positive integer generator

--- Random_Generators.h
+++ Random_Generators.h
@@ -1,5 +1,24 @@
+//------------------------------------------------------------------------------
+//Programmer:		Austin M Farrell
+//Function Definitions:
+//	
+//------------------------------------------------------------------------------
+#ifndef Random_Generators_h
+#define Random_Generators_h
+
 #include "Main.h"
 
+//------------------------------------------------------------------------------
+//GenerateIntegerFile: 
+//--------------------------------------
+//	quantity:
+//		
+//	minValue:
+//		
+//	maxValue:
+//		
+//------------------------------------------------------------------------------
+void GenerateIntegerFile(int quantity, int minValue, int maxValue);
 
 
-
+#endif // !Random_Generators_h
--- Random_Number_Generators.cpp
+++ Random_Number_Generators.cpp
@@ -1,3 +1,63 @@
+//------------------------------------------------------------------------------
+//Programmer:		Austin M Farrell
+//Function Definitions:
+//	
+//------------------------------------------------------------------------------
 #include "Random_Generators.h"
 
+//------------------------------------------------------------------------------
+//GenerateIntegerFile: 
+//--------------------------------------
+//	quantity:
+//		
+//	minValue:
+//		
+//	maxValue:
+//		
+//------------------------------------------------------------------------------
+void GeneratePositiveIntegerFile(int quantity, int minValue, int maxValue)
+{
+#pragma region Error_Testing
 
+	//Determine whether the given max value exceeds the limit
+	if (maxValue >= RAND_MAX)
+	{
+		//Use the limit as the max value
+		maxValue = RAND_MAX;
+	} // !if
+
+	//Determine whether the given min value exceeds the limit
+	if (minValue >= RAND_MAX)
+	{
+		//Set the min value to half the max value
+		minValue = (int)(maxValue / 2);
+	} // !if
+
+	//Determine whether the given max value is negative
+	if (maxValue < 0)
+	{
+		//Use the positive version of the given max value
+		maxValue = (maxValue * -1);
+	} // !if
+
+	//Determine whether the given min value is negative
+	if (minValue < 0)
+	{
+		//Use 0 as the min value
+		minValue = 0;
+	} // !if
+
+	//Determine whether the given min value is greater than the given max value
+	if (minValue >= maxValue)
+	{
+		//Set the min value to half the max value
+		minValue = (int)(maxValue / 2);
+	} // !if
+
+#pragma endregion
+
+	
+
+
+
+} // !GenerateIntegerFile
